93ba315391b921db294600619ee135c9cf227553
Ryan McMurtry <rkainoam@gmail.com>
2023-08-03 02:03:38 -0700

    changed edit component

--- front-end/src/edit/Edit.js
+++ front-end/src/edit/Edit.js
@@ -9,23 +9,15 @@ function Edit() {
   const [reservation, setReservation] = useState(null);
   const [resError, setResError] = useState(null);
 
-  // const loadReservation = () => {
-  //   const abortController = new AbortController();
-  //   readReservation(reservation_id, abortController.signal)
-  //     .then(setReservation)
-  //     .catch(setResError);
-  //   return () => abortController.abort();
-  // };
+  const loadReservation = () => {
+    const abortController = new AbortController();
+    readReservation(reservation_id, abortController.signal)
+      .then(setReservation)
+      .catch(setResError);
+    return () => abortController.abort();
+  };
 
-  useEffect(() => {
-    async function loadReservation() {
-      const abortController = new AbortController();
-      const response = await readReservation(reservation_id, abortController.signal)
-      setReservation(response);
-      return () => abortController.abort();
-    }
-    loadReservation();
-  }, [reservation_id]);
+  useEffect(loadReservation, [reservation_id]);
 
   return (
     <div>
